40cbe631223b510092a602c095bd41b249c25b44
Guillaume Perrin <guillaume28.perrin@gmail.com>
2014-06-02 19:58:33 +0200

    Add nodeJS subscriber and test with a publisher

--- Mosquitto/publisher-test/main.cpp
+++ Mosquitto/publisher-test/main.cpp
@@ -3,9 +3,11 @@
 #elif defined(_WIN32)
 #include <windows.h>
 #endif
+#include <stdio.h>
 
 #include "mqtt_sender.h"
 
+/* For test propose only !! */
 int main(int argc, char *argv[])
 {
 	class mqtt_sender *sender;
@@ -18,18 +20,25 @@ int main(int argc, char *argv[])
 #endif
 
 	sender = new mqtt_sender("sender", "localhost", 1883);
+    int i = 0;
 	
 	while(1){
 		rc = sender->loop();
 		if(rc){
 			sender->reconnect();
 		}
-        sender->send_message("presence", "Hello mqtt");
+        char b[50];
+        sprintf(b, "%d", rand() % 180);
+        sender->send_message("metrics/steering", b);
+        sender->send_message("metrics/throttle", b);
 #if defined(__linux__) || defined(__APPLE__)
         sleep(1);
 #elif defined(_WIN32)
         Sleep(1000);
 #endif
+        i++;
+        if (i >= 180)
+            i = 0;
 	}
 
 #ifdef __linux__
