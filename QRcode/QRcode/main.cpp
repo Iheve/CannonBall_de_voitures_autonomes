436e3626959fcfe5b6d7790c801bd4ca25017f11
Tablette <coutelot@ensimag.grenoble-inp.fr>
2014-06-03 19:04:09 +0200

    refactoring main

--- QRcode/QRcode/main.cpp
+++ QRcode/QRcode/main.cpp
@@ -12,15 +12,85 @@
 using namespace cv;		
 using namespace aruco;
 
-std::string mqtt_server = "localhost";
+//Arduino
+Serial* arduin;
+char* port = "\\\\.\\COM5";
 
+//MQTT
 mqtt_sender *sender;
+char* mqtt_host = "localhost";
 
-void initArduino(Serial** arduin, char * port) {
-	*arduin = new Serial(port); // adjust as needed
+//Current state
+int steering = 90;
+int throttle = 91;
 
-	if ((*arduin)->IsConnected())
+//aruco
+VideoCapture TheVideoCapturer;
+Mat TheInputImage, TheInputImageCopy;
+vector<Marker> TheMarkers;
+CameraParameters TheCameraParameters;
+MarkerDetector MDetector;
+Dictionary D;
+String TheDict = "dict.yml";
+String TheCamParam = "camparam.yml";
+float TheMarkerSize = 0.089f;
+
+
+/**
+ * Initialization of the arduino
+ */
+void initArduino() {
+	arduin = new Serial(port);
+
+	if (arduin->IsConnected())
 		std::cout << "Connection to the arduino OK" << std::endl;
+}
+
+void initMQTT() {
+	std::cout << "Connecting to " << mqtt_host << std::endl;
+	sender = new mqtt_sender("sender", mqtt_host, 1883);
+	sender->publish_to_mqtt("presence", "Hello mqtt");
+}
+
+void initAruco() {
+	//Open webcam
+	TheVideoCapturer.open(0);
+	TheVideoCapturer.set(CV_CAP_PROP_FRAME_WIDTH, 1920 / 3);
+	TheVideoCapturer.set(CV_CAP_PROP_FRAME_HEIGHT, 1080 / 3);
+	if (!TheVideoCapturer.isOpened()) {
+		cerr << "Could not open video" << std::endl;
+		exit(-1);
+	}
+
+	//Load dictionary
+	if (D.fromFile(TheDict) == false) {
+		cerr << "Could not open dictionary" << std::endl;
+		exit(-1);
+	}
+	if (D.size() == 0) {
+		cerr << "Invalid dictionary" << std::endl;
+		exit(-1);
+	}
+	HighlyReliableMarkers::loadDictionary(D);
+
+	//read first image to get the dimensions
+	TheVideoCapturer >> TheInputImage;
+
+	//read camera parameters if passed
+	TheCameraParameters.readFromXMLFile(TheCamParam);
+	TheCameraParameters.resize(TheInputImage.size());
+
+	//Configure other parameters
+	//if (ThePyrDownLevel > 0)
+	//	MDetector.pyrDown(ThePyrDownLevel);
+
+	MDetector.setMakerDetectorFunction(aruco::HighlyReliableMarkers::detect);
+	MDetector.setThresholdParams(21, 7);
+	MDetector.setCornerRefinementMethod(aruco::MarkerDetector::LINES);
+	MDetector.setWarpSize((D[0].n() + 2) * 8);
+	MDetector.setMinMaxSize(0.005f, 0.5f);
+
+	cv::namedWindow("in", 1);
 }
 
 /**
@@ -45,91 +115,33 @@ void sendMetrics(int steering, int throttle, double laps, double avg) {
 	sender->publish_to_mqtt(TOPIC_THROT, (char*)std::to_string(throttle).c_str());
 	sender->publish_to_mqtt(TOPIC_LAPS, (char*)std::to_string(laps).c_str());
 	sender->publish_to_mqtt(TOPIC_AVG, (char*)std::to_string(avg).c_str());
-	cout << "SENT!" << endl;
-
+	//cout << "SENT!" << endl;
 }
 
+
+
 int main(int argc, char *argv[]) {
+	
+	//Arduino
+	initArduino();
+
+	//MQTT
 	mosqpp::lib_init();
-	if (argc >= 2) {
-		std::cout << "Connecting to " << argv[1] << std::endl;
-		sender = new mqtt_sender("sender", argv[1], 1883);
-	}
-	else {
-		std::cout << "Connecting to localhost" << std::endl;
-		sender = new mqtt_sender("sender", "localhost", 1883);
-	}
+	initMQTT();
 
-	sender->publish_to_mqtt("presence", "Hello mqtt");
-
-	int steering = 90;
-	int throttle = 91;
-	
-	Serial* arduin;
-	initArduino(&arduin, "\\\\.\\COM5");
-
-	String TheDict = "dict.yml";
-	String TheCamParam = "camparam.yml";
-	float TheMarkerSize = 0.089;
-
-	VideoCapture TheVideoCapturer;
-	vector<Marker> TheMarkers;
-	Mat TheInputImage, TheInputImageCopy;
-	CameraParameters TheCameraParameters;
-	MarkerDetector MDetector;
-	
-	TheVideoCapturer.open(0);
-	TheVideoCapturer.set(CV_CAP_PROP_FRAME_WIDTH, 1920/3);
-	TheVideoCapturer.set(CV_CAP_PROP_FRAME_HEIGHT, 1080/3);
-
-	if (!TheVideoCapturer.isOpened()) {
-		cerr << "Could not open video" << std::endl;
-		return -1;
-	}
-
-	Dictionary D;
-	if (D.fromFile(TheDict) == false) {
-		cerr << "Could not open dictionary" << std::endl;
-		return -1;
-	};
-	if (D.size() == 0) {
-		cerr << "Invalid dictionary" << std::endl;
-		return -1;
-	};
-
-	HighlyReliableMarkers::loadDictionary(D);
-
-
-	//read first image to get the dimensions
-	TheVideoCapturer >> TheInputImage;
-
-	//read camera parameters if passed
-	TheCameraParameters.readFromXMLFile(TheCamParam);
-	TheCameraParameters.resize(TheInputImage.size());
-
-	//Configure other parameters
-	//if (ThePyrDownLevel > 0)
-	//	MDetector.pyrDown(ThePyrDownLevel);
-
-
-	MDetector.setMakerDetectorFunction(aruco::HighlyReliableMarkers::detect);
-	MDetector.setThresholdParams(21, 7);
-	MDetector.setCornerRefinementMethod(aruco::MarkerDetector::LINES);
-	MDetector.setWarpSize((D[0].n() + 2) * 8);
-	MDetector.setMinMaxSize(0.005, 0.5);
-
-	cv::namedWindow("in", 1);
+	//Aruco
+	initAruco();
 
 	IARabbit ia;
-
 	int index = 0;
 	double tick = (double)getTickCount();
 	double laps = 0;
 	float total = 0;
 	do {
+		//Calculate processing time
 		index++;
 		laps = (((double)getTickCount() - tick) / getTickFrequency() * 1000);
-		total += laps;
+		total += (float) laps;
 		std::cout << "time enlapsed : " << laps << " ms" << " (avg : " << total / index << ")" << std::endl;
 		tick = (double)getTickCount();
 
@@ -153,7 +165,7 @@ int main(int argc, char *argv[]) {
 		sendCommand(&arduin, steering, throttle);
 
 		//Send metrics to the MQTT brocker
-		//sendMetrics(steering, throttle, laps, total/index);
+		sendMetrics(steering, throttle, laps, total/index);
 
 		//show input with augmented information
 		cv::imshow("in", TheInputImageCopy);
